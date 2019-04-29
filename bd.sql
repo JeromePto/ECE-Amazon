44a27b6a9fee3317c9f3bce94d6df107ca3325d5
JeromePto <petot.jerome@gmail.com>
2019-04-29 12:39:51 +0200

    bd ameliorer

--- bd.sql
+++ bd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 29 avr. 2019 à 09:37
--- Version du serveur :  5.7.21
--- Version de PHP :  5.6.35
+-- Généré le :  lun. 29 avr. 2019 à 10:39
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,6 +36,26 @@ CREATE TABLE IF NOT EXISTS `acheteur` (
   `MAIL` varchar(255) NOT NULL,
   `MDP` text NOT NULL,
   `ADRESSE` varchar(255) NOT NULL,
+  `PANIER` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `STOCK` int(11) NOT NULL,
+  `NOM` varchar(255) NOT NULL,
+  `PHOTO` text NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `CATEGORIE` int(11) NOT NULL,
+  `PRIX` int(11) NOT NULL,
+  `VARIATION` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
