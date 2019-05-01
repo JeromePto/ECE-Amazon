f86574ca91745121ca9d668653748d56613e6072
Matquiniou <mathieu.quiniou@edu.ece.fr>
2019-05-01 14:02:22 +0200

    Blindage du mail, suppression de form_inscription.html et remplacement par form_inscription.php

--- cible.php
+++ cible.php
@@ -1,56 +1,102 @@
 <?php
 try
 {
-	// On se connecte à MySQL
+				// On se connecte à MySQL
 	$bdd = new PDO('mysql:host=localhost;dbname=bd;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
-	// En cas d'erreur, on affiche un message et on arrête tout
+				// En cas d'erreur, on affiche un message et on arrête tout
 	die('Erreur : '.$e->getMessage());
 }
 
-//echo "inscris !";
 if (isset($_POST['mail']) && isset($_POST['mdp'])&& isset($_POST['prenom'])&& isset($_POST['nom'])&& isset($_POST['adresse']))
 {
 	$pass_hache = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
+
 	if($_POST['choix']==1)
 	{
-		echo "c'est un acheteur";
-		$req = $bdd->prepare('INSERT INTO acheteur(NOM, PRENOM, MAIL, MDP, ADRESSE) VALUES(:nom, :prenom, :mail, :mdp, :adresse)');
-		$req->execute(array(
-			'nom' => $_POST['nom'], 
-			'prenom' => $_POST['prenom'],
-			'mail' => $_POST['mail'],
-			'mdp' => $pass_hache,
-			'adresse' => $_POST['adresse'],
-		));
+		$reponse = $bdd->query('SELECT * FROM acheteur');
+
+		$verif=1;
+
+		while ($donnees = $reponse->fetch())
+		{
+			if ($donnees['MAIL']==$_POST['mail']) 
+			{
+				$verif=2;
+			}
+		}
+
+		$reponse->closeCursor();
+
+		if ($verif==1) 
+		{
+			$req = $bdd->prepare('INSERT INTO acheteur(NOM, PRENOM, MAIL, MDP, ADRESSE) VALUES(:nom, :prenom, :mail, :mdp, :adresse)');
+			$req->execute(array(
+				'nom' => $_POST['nom'], 
+				'prenom' => $_POST['prenom'],
+				'mail' => $_POST['mail'],
+				'mdp' => $pass_hache,
+				'adresse' => $_POST['adresse'],
+			));
+
+			session_start ();
+
+			$_SESSION['id'] = $bdd->query("SELECT * FROM acheteur ORDER BY ID DESC")->fetch()["ID"];
+			$_SESSION['mail'] = $_POST['mail'];
+			$_SESSION['nom'] = $_POST['nom'];
+			$_SESSION['prenom'] = $_POST['prenom'];
+			$_SESSION['adresse'] = $_POST['adresse'];
+
+			header ('location: Acheteur.php');
+		}
+		else
+		{
+			$erreur=10;
+			header("location: form_inscription.php?erreur={$erreur}");
+		}
 	}
 	if($_POST['choix']==2)
 	{
-		$req = $bdd->prepare('INSERT INTO vendeur(NOM, PRENOM, MAIL, MDP, ADRESSE) VALUES(:nom, :prenom, :mail, :mdp, :adresse)');
-		$req->execute(array(
-			'nom' => $_POST['nom'], 
-			'prenom' => $_POST['prenom'],
-			'mail' => $_POST['mail'],
-			'mdp' => $pass_hache,
-			'adresse' => $_POST['adresse'],
-		));
-		echo "c'est un vendeur";
+		$reponse2 = $bdd->query('SELECT * FROM vendeur');
+
+		$verif2=1;
+
+		while ($donnees2 = $reponse2->fetch())
+		{
+			if ($donnees2['MAIL']==$_POST['mail']) 
+			{
+				$verif2=2;
+			}
+		}
+		$reponse2->closeCursor();
+		if ($verif2==1) 
+		{
+			$req2 = $bdd->prepare('INSERT INTO vendeur(NOM, PRENOM, MAIL, MDP, ADRESSE) VALUES(:nom, :prenom, :mail, :mdp, :adresse)');
+			$req2->execute(array(
+				'nom' => $_POST['nom'], 
+				'prenom' => $_POST['prenom'],
+				'mail' => $_POST['mail'],
+				'mdp' => $pass_hache,
+				'adresse' => $_POST['adresse'],
+			));
+
+			session_start ();
+
+			$_SESSION['id'] = $bdd->query("SELECT * FROM vendeur ORDER BY ID DESC")->fetch()["ID"];
+			$_SESSION['mail'] = $_POST['mail'];
+			$_SESSION['nom'] = $_POST['nom'];
+			$_SESSION['prenom'] = $_POST['prenom'];
+			$_SESSION['adresse'] = $_POST['adresse'];
+			header ('location: Vendeur.php');
+		}
+		else
+		{
+			$erreur=10;
+			header("location: form_inscription.php?erreur={$erreur}");
+		}
 	}
-
-
-	session_start ();
-			// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
-	$_SESSION['id'] = $bdd->query("SELECT * FROM acheteur ORDER BY ID DESC")->fetch()["ID"];
-	$_SESSION['mail'] = $_POST['mail'];
-	$_SESSION['nom'] = $_POST['nom'];
-	$_SESSION['prenom'] = $_POST['prenom'];
-	$_SESSION['adresse'] = $_POST['adresse'];
-
-			// on redirige notre visiteur vers une page de notre section membre
-	//header ('location: Acheteur.php');
-
-}
+}	
 ?>
