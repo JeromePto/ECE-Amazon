0b80a658e62d343afc482c7b796d07eb4947487e
TreyPrase <batpillet@gmail.com>
2019-05-03 16:11:59 +0200

    Probleme image

--- Sport.php
+++ Sport.php
@@ -160,7 +160,7 @@ $reponse = $bdd->query('SELECT * FROM item WHERE CATEGORIE=3 ');
 										<div class="card card-product">
 											<div class="card-image">
 												<a href=<?php echo("produit.php?id=".$id)?>>
-													<img alt="Image placeholder" src=<?php echo($donnees['PHOTO'])?> class="img-center img-fluid">
+													<img alt="Image placeholder" src=<?php echo('images/'.$donnees['PHOTO'])?> class="img-center img-fluid">
 												</a>
 											</div>
 											<div class="card-body text-center pt-0">
